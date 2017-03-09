b3ed14a0aeff1db79f27edeaab374d5ebcc43e31
abgatov <alexbgatov@gmail.com>
2017-03-09 14:40:05 -0800

    Update Obstacle.java

--- Obstacle.java
+++ Obstacle.java
@@ -2,18 +2,18 @@ import java.awt.Color;
 
 public abstract class Obstacle extends GridObject
 {
-	/* Directions that GridObjects can point */
-	public static final int NONE = 0;
-	public static final int UP = 1;
-	public static final int DOWN = 2;
-	public static final int LEFT =  3;
-	public static final int RIGHT =  4;
-	protected String [] dirSymbols = {"#","^","v","<",">"}; 
+  /* Directions that GridObjects can point */
+  public static final int NONE = 0;
+  public static final int UP = 1;
+  public static final int DOWN = 2;
+  public static final int LEFT =  3;
+  public static final int RIGHT =  4;
+  protected String [] dirSymbols = {"#","^","v","<",">"}; 
 
   protected Coord location;
   protected String symbol = null;
   protected Color color;	
-	protected int dir = NONE;
+  protected int dir = NONE;
 
   public Obstacle()
   {
